fc5e6885aed71ff716acdd3ddd21bdfa9da53ee7
Dayvid <dayvidmac18@gmail.com>
2022-07-16 15:54:55 -0300

    aprendizado array e objetos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Button from './components/button/Button';
 function App() {
+  let contatos = [{ nome: "daniel", cidade: "juazeiro" }, { nome: "dayvid", cidade: "ipaba" }]
+
   return (
     <div className="App">
       <header className="App-header">
+      {contatos.map((contato,index)=>{return <div key={index}>{contato.nome}-{contato.cidade}</div>})}
         <Button tipo="save"> algo aqui
         </Button>
+  
       </header>
     </div>
   );
